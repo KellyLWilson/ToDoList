da84ffa5da2a480357b9dbe22c616a880290ecf1
KellyLWilson <60358839+KellyLWilson@users.noreply.github.com>
2020-03-27 08:22:00 -0400

    yuck

--- src/Todolist.js
+++ src/Todolist.js
@@ -9,14 +9,58 @@ class ToDoList extends React.Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.clearAll = this.clearAll.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.checkBoxed=this.checkBoxed.bind(this);
+    this.setView = this.setView.bind(this);
+    //this.checkHandler = this.checkHandler.bind(this)
     this.state = {
         list: '',
         items: [],
-        archiveItems: [],
-        
-      
+        view: 'inprogress',
+        title: 'To-do'
     };
   }
+
+setView(e) {
+  e.preventDefault()
+
+  let tempView = 'all'
+  let tempTitle = 'To-do'
+
+  if(e.target.id === "done") {
+    tempView = "done"
+    tempTitle = "Done"
+  }
+  else if (e.target.id === "inprogress") {
+    tempView = "inprogress"
+    tempTitle = "To-do"
+  }
+  else if (e.target.id = "all") {
+    tempView = "all"
+    tempTitle = "All items"
+  }
+  this.setState({
+    view: tempView,
+    title: tempTitle
+  })
+}
+
+
+  checkBoxed (id, checked) {
+    const newArr = this.state.items.map((item, index) => id={index})
+    if (this.state.items, id === id) {
+      DataTransferItemList.checked = checked 
+    }
+return this.state.item
+}
+
+
+// this.setState({
+//   items: newArr.filter(item =>  (item.checked === ))
+// })
+
+
+
 
 
 //local storage life cycle stuff   
@@ -41,7 +85,8 @@ var newList = JSON.parse(localStorage.getItem('items')) || [];
 
     componentDidUpdate () {
       localStorage.setItem('items', JSON.stringify(this.state.items));
-     
+      //list: window.localStorage.setItem("list", JSON.stringify(list));
+      //items: window.localStorage.setItem("items", JSON.stringify(items));
       //I think I need to send stuff to items.....array.push?
       
     }
@@ -76,6 +121,57 @@ clearAll (e) {
   
 }
 
+// addItem() {
+//   if (this.state.text.slice() !== '') {
+//     const text = {
+//       id: Date.now(),
+//       value: this.state.text.slice(),
+//       checked: false,
+//     };
+//     const list = [...this.state.list]
+
+//     list.push(text);
+
+//     this.setState({
+//       list,
+//       text: "",
+//       total: this.state.list.length + 1,
+//     })
+//   }
+//   }
+
+deleteItem() {
+  
+
+  localStorage.removeItem('items');
+
+//   deleteItem(id) {
+//     this.setState({
+//         todo: this.state.items.filter(el => el !== id)
+//     })
+ }
+
+  //var filteredItems = this.state.items.filter(function (item) {
+//     return (item.key !== key);
+//   });
+// }
+
+// const list = [...this.state.list]
+// //const updateList = [] 
+// list.map(item => {
+  
+//   if (item.id !== id) {
+//   //updatedList.push(item)
+//  }
+// })
+
+// this.setState({
+//   //    list: updatedList,
+//   total: this.state.list.length - 1,
+// })
+// window.localStorage.setItem('list', JSON.stringify)
+// };
+
   //stuff I tried first for local storage
 // saveToLocal = () => {
 //     const list = this.state.list;
@@ -90,6 +186,16 @@ clearAll (e) {
 //add a button to clear all
 //add an archive array to keep all the items
 
+// checkHandler = id => {
+//   this.setState ({
+//     items: this.state.items.map (items => {
+//       if (items.id === id) {
+//         items.checked = !items.checked;
+//       }
+//       return items;
+//     }),
+//   });
+// };
 
 
 
@@ -99,13 +205,22 @@ clearAll (e) {
       <div>
           <h2>To Do List</h2>
           
+          <button className= "bg-info">All</button>
+          
+          <button className= "bg-info">Completed</button>
+          <button className= "bg-info">In Progress</button>
+        
+          
       </div>
         <form className="To Do List" onSubmit={this.onSubmit}>
           <input value={this.state.list} onChange={this.onChange} />
-          <button>Submit</button>
-          <button onClick={this.clearAll}>Clear All</button>
-        </form>
-        <Display items={this.state.items} />
+          <button className= "bg-success">Submit</button>
+         
+          
+        <span ><Display items={this.state.items} /></span>
+        <button className= "bg-warning" onClick={this.deleteItem}>Delete</button>  
+        <button className= "bg-danger" onClick={this.clearAll}>Clear All</button>
+          </form>
       </div>
     );
   }
@@ -114,17 +229,20 @@ clearAll (e) {
 
 //probably and easier way to do this but setting a constant to display 
 //the output of the list and then calling it in the export default
-let Display = props => (
-    <div className="border">
-    <ul>
-      {
-        props.items.map((item, index) => <li key={index}>{item}</li>)
-      }
-    </ul>
-    </div>
-  );
-  
-  export default Display;
+
+
+const Display = props => (
+  <div className="border">
+  <ul>
+    {
+      props.items.map((item, index) => <li key={index}>{item} <input type="checkbox" aria-label="Checkbox for following text input"></input></li>)
+    }
+    
+  </ul>
+  </div>
+);
+
+export default Display;
 
 
 
